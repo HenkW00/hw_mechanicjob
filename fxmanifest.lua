a81e1bcfbcd73d5f55f4f1f4fbfa7981dab2ca86
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-05-08 23:58:04 +0200

    Fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,10 @@
 fx_version 'bodacious'
-lua54 'yes'
 game  'gta5'
+lua54 'yes'
 
 author 'HenkW'
 description 'Advanced mechanic job using OX'
-version '1.0.0'
+version '1.0.1'
 
 client_scripts{
 	'client/client.lua', 
@@ -28,5 +28,12 @@ shared_scripts {
 
 dependencies {
 	'oxmysql',
-	'ox_lib'
+	'ox_lib',
+	'hw_utils'
 }
+
+escrow_ignore {
+    'config.lua',
+    'fxmanifest.lua',
+    'README.MD'
+}server_scripts { '@mysql-async/lib/MySQL.lua' }
